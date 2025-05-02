c24e1c2bf02999e18c57278c73e60b8c6eee12d2
goutham <uppinurigouthamreddy@gmail.com>
2025-05-02 22:44:09 +0530

    test server file added

---
+++ test.py
@@ -0,0 +1,9 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route("/" , methods=['GET'])
+def root() :
+    return "Hi test server running"
+
+app.run( host='0.0.0.0' , port=4000 , debug=True )
